af1eac23e815a580a674370c567df3e417a27ef8
jakecomp <jakobvalen9@gmail.com>
2023-06-05 10:47:34 -0700

    Fix random number problem, but want range of random numbers created to increase

--- generatePrime.c
+++ generatePrime.c
@@ -1,22 +1,36 @@
 #include <stdio.h> 
 #include <string.h> 
 #include <stdlib.h>   
-#include <math.h>
+#include <math.h>  
+#include <time.h>
+
+#define NUM_BITS 63
 
 // Generates a random number in the range of desired bit size for our 
 // 2 prime numbers
 long long random_nums(){ 
 
-    long long upper_limit = (long long)pow(2,2048) - 1;  
-    long long lower_limit = (long long)pow(2,2047) + 1;
-    printf("%lld \n",upper_limit); 
-    printf("%lld \n",lower_limit);
+    long long upper_limit = (long long)pow(2,NUM_BITS) - 1;  
+    long long lower_limit = (long long)pow(2,NUM_BITS - 1) + 1;
+    srand(time(NULL)); 
+
+    printf("Upper Limit is: %lld \n", upper_limit); 
+    printf("Lower limit is: %lld \n", lower_limit);  
+    printf("Difference: %lld \n",upper_limit - lower_limit);
+    printf("#################################### \n");
     return lower_limit + (rand() % (upper_limit - lower_limit + 1));
 }
 
 int main() { 
 
-    long long my_num = random_nums();
-    printf("%lld \n",my_num);
-    return my_num;
+    long long first_rand = random_nums(); 
+    long long second_rand = random_nums(); 
+    
+    while(first_rand == second_rand){ 
+        second_rand = random_nums();
+    } 
+
+    printf("First random number is: %lld \n", first_rand); 
+    printf("Second random number is: %lld \n", second_rand);
+    return 1;
 }
