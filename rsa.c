46eec86023d1f43bcca3184e49d5ab353d0a0121
Josh Berryman <jberryman566@gmail.com>
2023-07-03 10:58:53 -0700

    Rewrote RSA to be simpler. Additionally added a timer for reporting encryption times. Note: its fast! Need to find a way to report smaller times.

--- rsa.c
+++ rsa.c
@@ -1,145 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
+#include <time.h>
 
-int x, y, n, t, i, flag;
-long int e[50], d[50], temp[50], j, m[50], en[50];
-char msg[100];
-int prime(long int);
-void encryption_key();
-long int cd(long int);
-void encrypt();
-void decrypt();
+// Function to calculate the greatest common divisor (GCD) of two numbers
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+// Function to calculate the modular multiplicative inverse of a number (d) modulo phi(n)
+int modInverse(int d, int phi)
+{
+    int x, y;
+    int gcd_result = extendedGCD(d, phi, &x, &y);
+    if (gcd_result != 1)
+    {
+        printf("Error: Inverse does not exist.\n");
+        exit(1);
+    }
+    return (x % phi + phi) % phi;
+}
+
+// Extended Euclidean algorithm to calculate the modular multiplicative inverse
+int extendedGCD(int a, int b, int *x, int *y)
+{
+    if (b == 0)
+    {
+        *x = 1;
+        *y = 0;
+        return a;
+    }
+
+    int x1, y1;
+    int gcd_result = extendedGCD(b, a % b, &x1, &y1);
+
+    *x = y1;
+    *y = x1 - (a / b) * y1;
+
+    return gcd_result;
+}
+
+// Function to perform modular exponentiation
+int modularExponentiation(int base, int exponent, int modulus)
+{
+    if (modulus == 1)
+        return 0;
+
+    int result = 1;
+    base = base % modulus;
+
+    while (exponent > 0)
+    {
+        if (exponent & 1)
+            result = (result * base) % modulus;
+        exponent = exponent >> 1;
+        base = (base * base) % modulus;
+    }
+
+    return result;
+}
+
+// Function to encrypt an integer using the RSA algorithm
+int rsaEncrypt(int plaintext, int e, int n)
+{
+    return modularExponentiation(plaintext, e, n);
+}
+
+// Function to decrypt a ciphertext using the RSA algorithm
+int rsaDecrypt(int ciphertext, int d, int n)
+{
+    return modularExponentiation(ciphertext, d, n);
+}
 
 int main()
 {
-    printf("\nENTER FIRST PRIME NUMBER\n");
-    scanf("%d", &x);
-    flag = prime(x);
-    if (flag == 0)
-    {
-        printf("\nINVALID INPUT\n");
-        exit(0);
-    }
-    printf("\nENTER SECOND PRIME NUMBER\n");
-    scanf("%d", &y);
-    flag = prime(y);
-    if (flag == 0 || x == y)
-    {
-        printf("\nINVALID INPUT\n");
-        exit(0);
-    }
-    printf("\nENTER MESSAGE OR STRING TO ENCRYPT\n");
+    int p, q, n, phi, e, d;
+    int plaintext, ciphertext, decrypted;
 
-    scanf("%s", msg);
-    for (i = 0; msg[i] != NULL; i++)
-        m[i] = msg[i];
-    n = x * y;
-    t = (x - 1) * (y - 1);
-    encryption_key();
-    printf("\nPOSSIBLE VALUES OF e AND d ARE\n");
-    for (i = 0; i < j - 1; i++)
-        printf("\n%ld\t%ld", e[i], d[i]);
-    encrypt();
-    decrypt();
+    //srand(time(NULL));
+
+    // Step 1: Generate two distinct prime numbers (p and q)
+    p = 53; // Replace with your desired prime number
+    q = 59; // Replace with your desired prime number
+    printf("Generated prime numbers: p=%d, q=%d\n", p, q);
+
+    // Step 2: Calculate n and phi(n)
+    n = p * q;
+    phi = (p - 1) * (q - 1);
+
+    // Step 3: Choose a value for e such that 1 < e < phi(n) and gcd(e, phi(n)) = 1
+    e = 3; // Replace with your desired value for e
+    if (gcd(e, phi) != 1)
+    {
+        printf("Error: gcd(e, phi(n)) != 1\n");
+        return 1;
+    }
+
+    // Step 4: Calculate d, the modular multiplicative inverse of e modulo phi(n)
+    d = modInverse(e, phi);
+
+    printf("Public key: (n=%d, e=%d)\n", n, e);
+    printf("Private key: (n=%d, d=%d)\n", n, d);
+
+    // Step 5: Encrypt the plaintext
+    printf("Enter a number to encrypt: ");
+    scanf("%d", &plaintext);
+
+    clock_t start_time, end_time;
+    double execution_time;
+
+    start_time = clock();
+    ciphertext = rsaEncrypt(plaintext, e, n);
+    end_time = clock();
+
+    execution_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    printf("Encrypted value: %d\n", ciphertext);
+    printf("Encryption Time: %f seconds\n", execution_time);
+
+    // Step 6: Decrypt the ciphertext
+    decrypted = rsaDecrypt(ciphertext, d, n);
+    printf("Decrypted value: %d\n", decrypted);
+
     return 0;
-}
-int prime(long int pr)
-{
-    int i;
-    j = sqrt(pr);
-    for (i = 2; i <= j; i++)
-    {
-        if (pr % i == 0)
-            return 0;
-    }
-    return 1;
-}
-
-// function to generate encryption key
-void encryption_key()
-{
-    int k;
-    k = 0;
-    for (i = 2; i < t; i++)
-    {
-        if (t % i == 0)
-            continue;
-        flag = prime(i);
-        if (flag == 1 && i != x && i != y)
-        {
-            e[k] = i;
-            flag = cd(e[k]);
-            if (flag > 0)
-            {
-                d[k] = flag;
-                k++;
-            }
-            if (k == 99)
-                break;
-        }
-    }
-}
-long int cd(long int a)
-{
-    long int k = 1;
-    while (1)
-    {
-        k = k + t;
-        if (k % a == 0)
-            return (k / a);
-    }
-}
-
-// function to encrypt the message
-void encrypt()
-{
-    long int pt, ct, key = e[0], k, len;
-    i = 0;
-    len = strlen(msg);
-    while (i != len)
-    {
-        pt = m[i];
-        pt = pt - 96;
-        k = 1;
-        for (j = 0; j < key; j++)
-        {
-            k = k * pt;
-            k = k % n;
-        }
-        temp[i] = k;
-        ct = k + 96;
-        en[i] = ct;
-        i++;
-    }
-    en[i] = -1;
-    printf("\n\nTHE ENCRYPTED MESSAGE IS\n");
-    for (i = 0; en[i] != -1; i++)
-        printf("%c", en[i]);
-}
-
-// function to decrypt the message
-void decrypt()
-{
-    long int pt, ct, key = d[0], k;
-    i = 0;
-    while (en[i] != -1)
-    {
-        ct = temp[i];
-        k = 1;
-        for (j = 0; j < key; j++)
-        {
-            k = k * ct;
-            k = k % n;
-        }
-        pt = k + 96;
-        m[i] = pt;
-        i++;
-    }
-    m[i] = -1;
-    printf("\n\nTHE DECRYPTED MESSAGE IS\n");
-    for (i = 0; m[i] != -1; i++)
-        printf("%c", m[i]);
-    printf("\n");
 }
